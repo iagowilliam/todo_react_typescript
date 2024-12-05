b1b0c79078b7500ad43dacbfd79aa62eb2cc3845
Iago William <iagowilliam@hotmail.com>
2024-12-05 18:01:03 -0300

    Criado header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><span>React + TS Todo</span> @ 2024</p>
+        </footer>
+    )
+}
+
+export default Footer
