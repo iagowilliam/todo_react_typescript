688c672de8c2a2c0a75c27115ac73f882537da8c
Iago William <iagowilliam@hotmail.com>
2024-12-05 20:36:44 -0300

    Feito a adição de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // components
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -12,6 +14,8 @@ import { ITask } from './interfaces/Task'
 
 function App() {
 
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <>
       <div>
@@ -19,7 +23,7 @@ function App() {
         <main className={styles.main}>
           <div>
             <h2>O que você vai fazer?</h2>
-            <TaskForm btnText='Criar Tarefa'/>
+            <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
           </div>
           <div>
             <h2>Suas tarefas:</h2>
