6c8f2cfb860acc08d8492160f7c3ef59b73c8f30
Iago William <iagowilliam@hotmail.com>
2024-12-06 19:32:23 -0300

    Feito a exibição da lista de tarefas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
-interface Props {}
+// interface
+import { ITask } from '../interfaces/Task'
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from "./TaskList.module.css"
+
+interface Props {
+  taskList: ITask[]
+}
+
+const TaskList = ({taskList}: Props) => {
   return (
-    <div>
-        <p>Lista tarefa</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <p>{task.title}</p>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
   )
 }
 
